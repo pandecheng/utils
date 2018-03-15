363c99db2e1e66f42af45d5981f8d6151a51fdb1
pandecheng <pandechenge@163.com>
2018-03-15 15:44:38 +0800

    Update utils_2018_3_15.js

--- utils_2018_3_15.js
+++ utils_2018_3_15.js
@@ -4,3 +4,16 @@
     const average = arr => arr.reduce((acc, val) => acc + val, 0) / arr.length;
     
     //average([1,2,3,4,5]) -> 3
+
+2 大写每个单词的首字母
+    使用replace() 匹配每个单词的第一个字符，并使用toUpperCase() 
+    
+   const capitalizeEveryWord = str => str.replace(/\b[a-z]/g,char => char.toUpperCase());
+    
+    //capitalizeEveryWord('hi ming'); -> 'Hi Ming'
+3 首字母大字
+    使用slice(1)
+    
+    const capitalize = str => str[0].toUpperCase() + str.slice(1);
+
+    //capitalize('hi'); -> 'Hi'
